38eb09cac9d751daea4ccd1cc4990c36619cdda4
danukaj <damayanthiamarakoon753@gmail.com>
2023-09-04 23:20:22 +0530

    1st

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LK.com Sign in || Sign Up</title>
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class=" container-fluid">
+        <div class="row">
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </div>
+    <script src="bootstrap.css"></script>
+    <script src=""></script>t
+</body>
+</html>
