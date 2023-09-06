c47ac110573f9667462648015f13168a0305b9b7
danukaj <damayanthiamarakoon753@gmail.com>
2023-09-06 18:13:55 +0530

    script and caegory

--- home.php
+++ home.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,6 +10,7 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="font.css">
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="row">
@@ -16,22 +18,104 @@
             require "connection.php";
             require "header.php";
             ?>
-            <?php            
+            <?php
             require "search.php";
             ?>
-                <div class="col-12">
-                    
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="row">
+                            <div id="myDiv" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-1</span>
+                            </div>
+                            <div id="myDiv2" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-2</span>
+                            </div>
+                            <div id="myDiv3" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-3</span>
+                            </div>
+                            <div id="myDiv4" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-4</span>
+                            </div>
+                            <div id="myDiv5" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-5</span>
+                            </div>
+                            <div id="myDiv6" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-6</span>
+                            </div>
+                            <div id="myDiv7" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-7</span>
+                            </div>
+                            <div id="myDiv8" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-8</span>
+                            </div>
+                            <div id="myDiv9" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-9</span>
+                            </div>
+                            <div id="myDiv10" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-10</span>
+                            </div>
+                            <div id="myDiv11" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-11</span>
+                            </div>
+                            <div id="myDiv12" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-12</span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-6">
+                        <div class="row">
+                            <div id="myDiv13" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-13</span>
+                            </div>
+                            <div id="myDiv14" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-14</span>
+                            </div>
+                            <div id="myDiv15" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-15</span>
+                            </div>
+                            <div id="myDiv16" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-16</span>
+                            </div>
+                            <div id="myDiv17" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-17</span>
+                            </div>
+                            <div id="myDiv18" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-18</span>
+                            </div>
+                            <div id="myDiv19" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-19</span>
+                            </div>
+                            <div id="myDiv20" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-20</span>
+                            </div>
+                            <div id="myDiv21" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-21</span>
+                            </div>
+                            <div id="myDiv22" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-22</span>
+                            </div>
+                            <div id="myDiv23" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-23</span>
+                            </div>
+                            <div id="myDiv24" class="col-1 border-bottom text-center">
+                                <span class="fw-bold mb-3">Category-24</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            <?php
-            require "footer.php";
-            ?>
+                <?php
+                require "footer.php";
+                ?>
+            </div>
         </div>
-    </div>
-    <script src="script.js"></script>
-    <script src="bootstrap.bundle.js"></script>
-    <script src="bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js"></script>
+        <script src="script.js"></script>
+        <script src="bootstrap.bundle.js"></script>
+        <script src="bootstrap.bundle.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js"></script>
 </body>
+
 </html>
--- script.js
+++ script.js
@@ -186,3 +186,28 @@ function signOut(){
     r.send();
     
 }
+
+
+
+var newmodal;
+document.addEventListener("DOMContentLoaded", function() {
+    const myDiv = document.getElementById("myDiv");
+
+
+
+    // Function to be executed when the mouse enters the <div>
+    function handleMouseEnter() { // Change background color
+        myDiv.classList="col-1 border-top border-start border-end text-center";
+    }
+
+    // Function to be executed when the mouse leaves the <div>
+    function handleMouseLeave() {
+        myDiv.classList="col-1 text-center border-bottom"; // Reset background color
+    }
+
+    // Add a mouseenter event listener to the <div>
+    myDiv.addEventListener("mouseenter", handleMouseEnter);
+    // Add a mouseleave event listener to the <div>
+    myDiv.addEventListener("mouseleave", handleMouseLeave);
+});
+
